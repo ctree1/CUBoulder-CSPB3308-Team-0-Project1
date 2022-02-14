5de467cee14a0343bec759347a97e7bc0fee25a1
root <root@Puuter.localdomain>
2022-02-13 22:39:13 -0700

    Added bathroom and sleep example queries

---
+++ AddBathroom.sql
@@ -0,0 +1,2 @@
+-- SQLite
+INSERT INTO babies (birthDate, firstName, lastName) VALUES ("2022-01-01", "John", "Test");
---
+++ AddSleep.sql
@@ -0,0 +1,2 @@
+-- SQLite
+INSERT INTO sleep (babyID, sleepType, sleepTime, sleepComment) VALUES (1, "Wake", "2022-01-01 03:15", "Woke up screaming!");
