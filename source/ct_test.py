d128be722baa3474a86fae7527a1d71996aaba60
Connor <chtr2312@colorado.edu>
2022-02-15 21:47:02 -0700

    json to sql ready for testing

--- source/ct_test.py
+++ source/ct_test.py
@@ -1,6 +1,9 @@
 import sqlite3
+import os
 
 def json_to_sql(value):
+    os.chdir("sqlite3") #If someone can figure out the file path to baby.db, ../sqlite3/baby.db does not work
+
     conn = sqlite3.connect("baby.db")
     c = conn.cursor()
     babyID = value['babyId']
@@ -8,5 +11,6 @@ def json_to_sql(value):
     bathroomComment = value['comment']
     bathroomDateTime = value['time']
 
-    c.execute("INSERT INTO bathroom VALUES(?, ?, ?, ?, ?);", (babyID, bathroomType, bathroomDateTime, bathroomComment))
+    c.execute("INSERT INTO bathroom VALUES(?, ?, ?, ?);", (babyID, bathroomType, bathroomDateTime, bathroomComment))
     conn.close()
+    os.chdir("..")
