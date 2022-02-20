17792653fc52f327aa4e5f581558259e3db52300
Michael Lamelza <michael@pivotalfederal.com>
2022-02-19 21:31:20 -0500

    Finished main data handling. Controls are now fully functional. Minor enhancements still needed.

--- source/static/scripts/infanttracker.ts
+++ source/static/scripts/infanttracker.ts
@@ -1,28 +1,86 @@
+enum BathroomTypeEnum {
+    none = 0,
+    liquid = 1,
+    solid = 2,
+    both = 3
+}
+
 class BathroomEvent {
-    babyId: number;
-    bathroomType: string;
-    time: string
-    comment: string;
+    babyId: number = null;
+    type: BathroomTypeEnum = 0;
+    dateTime: string;
+    comment: string = ""
 }
 
 var bathroomEvent = new BathroomEvent();
+var timeElem = <HTMLInputElement>document.getElementById('inputTime');
+var btnLiquidElem = <HTMLInputElement>document.getElementById('btnLiquid');
+var btnSolidElem = <HTMLInputElement>document.getElementById('btnSolid');
+var btnBothElem = <HTMLInputElement>document.getElementById('btnBoth');
+var btnDoneElem = <HTMLInputElement>document.getElementById('btnBoth');
+var btnOneMoreElem = <HTMLInputElement>document.getElementById('btnBoth');
+var btnDoneElem = <HTMLInputElement>document.getElementById('btnDone');
+var btnOneMoreElem = <HTMLInputElement>document.getElementById('btnOneMore');
+var today: Date = new Date();
+var dateTime = toISOLocal(today);
+timeElem.value = dateTime;
+bathroomEvent.dateTime = dateTime;
+
+function updateSubmitBtns() {
+    if (bathroomEvent.babyId && bathroomEvent.babyId != 0 && bathroomEvent.type != BathroomTypeEnum.none) {
+        btnDoneElem.disabled = false;
+        btnOneMoreElem.disabled = false;
+    } else {
+        btnDoneElem.disabled = true;
+        btnOneMoreElem.disabled = true;
+    }
+}
 
 function selectBaby(babyId: number) {
     bathroomEvent.babyId = +babyId;
+    updateSubmitBtns();
+}
+
+function updateBathroomType(type: BathroomTypeEnum) {
+
+    bathroomEvent.type = bathroomEvent.type == type ? BathroomTypeEnum.none : type;
+    switch (bathroomEvent.type){
+        case BathroomTypeEnum.liquid:
+            btnLiquidElem.className = "btn btn-primary"
+            btnSolidElem.className = "btn btn-secondary"
+            btnBothElem.className = "btn btn-secondary"
+            break;
+        case BathroomTypeEnum.solid:
+            btnLiquidElem.className = "btn btn-secondary"
+            btnSolidElem.className = "btn btn-primary"
+            btnBothElem.className = "btn btn-secondary"
+            break;
+        case BathroomTypeEnum.both:
+            btnLiquidElem.className = "btn btn-secondary"
+            btnSolidElem.className = "btn btn-secondary"
+            btnBothElem.className = "btn btn-primary"
+            break;
+        default:
+            btnLiquidElem.className = "btn btn-secondary"
+            btnSolidElem.className = "btn btn-secondary"
+            btnBothElem.className = "btn btn-secondary"
+            break;
+    }
+    updateSubmitBtns();
+}
+
+function updateTime(time: string) {
+    bathroomEvent.dateTime = time;
 }
 
 function updateComment(comment: string) {
     bathroomEvent.comment = comment;
 }
 
-function addBathroomEvent(event: BathroomEvent) {
+function addBathroomEvent() {
     let xhr = new XMLHttpRequest();
     let url = "/bathroom";
     xhr.open("POST", url, true);
-    event.babyId = bathroomEvent.babyId ? bathroomEvent.babyId : event.babyId
-    event.bathroomType = bathroomEvent.bathroomType ? bathroomEvent.bathroomType : event.bathroomType
-    event.time = bathroomEvent.time ? bathroomEvent.time : event.time
-    event.comment = bathroomEvent.comment ? bathroomEvent.comment : event.comment
     xhr.setRequestHeader("Content-Type", "application/json");
     xhr.onreadystatechange = function () {
         if (xhr.readyState === 4 && xhr.status === 200) {
@@ -30,6 +88,18 @@ function addBathroomEvent(event: BathroomEvent) {
             window.location.reload();
         }
     };
-    var data = JSON.stringify({ "bathroomEvent": event });
+    var data = JSON.stringify({ "bathroomEvent": bathroomEvent });
     xhr.send(data);
 } 
+
+function toISOLocal(d: Date) {
+var z  = n =>  ('0' + n).slice(-2);
+var zz = n => ('00' + n).slice(-3);
+
+return d.getFullYear() + '-'
+        + z(d.getMonth()+1) + '-' +
+        z(d.getDate()) + 'T' +
+        z(d.getHours()) + ':'  + 
+        z(d.getMinutes()) + ':' +
+        z(d.getSeconds()); 
+}
