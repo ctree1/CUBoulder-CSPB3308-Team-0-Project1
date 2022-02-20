d0fbe62ade341d7adb224866f865aac93e82e846
Steve <stpu6983@colorado.edu>
2022-02-19 20:11:43 -0700

    db: bathroom and sleep types to int, +data type labels

--- CreateBathroomTypeTable.sql
+++ CreateBathroomTypeTable.sql
@@ -1,10 +1,5 @@
 -- SQLite
-CREATE TABLE bathroom (
-bathroomEventID INTEGER PRIMARY KEY AUTOINCREMENT,
-babyID INTEGER NOT NULL,
-bathroomType INTEGER NOT NULL,
-bathroomDateTime TEXT NOT NULL, -- YYYY-MM-DD HH:MM
-bathroomComment TEXT,
-FOREIGN KEY (babyID)
-    REFERENCES babies (babyID)
+CREATE TABLE bathroomType (
+bathroomTypeID INTEGER PRIMARY KEY AUTOINCREMENT,
+bathroomTypeName TEXT NOT NULL
 );
