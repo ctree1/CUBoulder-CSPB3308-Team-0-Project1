67e79701e8f17fdb743a2675376af96445fad074
Connor <chtr2312@colorado.edu>
2022-02-20 21:26:32 -0700

    Fixed minor formating

---
+++ source/sql_functions.py
@@ -0,0 +1,21 @@
+import sqlite3
+import os
+
+
+def bathroom_sql_ins(value):
+    os.chdir("sqlite3") #change working directory to that of database
+
+    conn = sqlite3.connect("baby.db")       #connect to database
+    cur = conn.cursor()   
+    babyID = value['babyId']        #split tuple into individual variables
+    bathroomType = value['type']
+    bathroomComment = value['comment']
+    bathroomDateTime = value['dateTime']
+
+    #execute sql insert statement
+    cur.execute("INSERT INTO bathroom (babyID, bathroomType, bathroomDateTime, bathroomComment) VALUES(?, ?, ?, ?);", (babyID, bathroomType, bathroomDateTime, bathroomComment))
+    conn.commit()
+    conn.close()
+    os.chdir("..")
+
+
