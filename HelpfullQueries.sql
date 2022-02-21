9c6686723ca0b826c8fee69d5e5bac2158b2f40a
Steve <stpu6983@colorado.edu>
2022-02-20 20:50:38 -0700

    New view for bathroom - client facing

--- HelpfullQueries.sql
+++ HelpfullQueries.sql
@@ -1,5 +1,8 @@
 -- SQLite
-SELECT * FROM sleep;
+--SELECT * FROM bathroom;
 --INSERT INTO sleepType (sleepTypeName) VALUES ("Asleep");
 --ALTER TABLE sleepType RENAME COLUMN sleepEventName TO sleepTypeName;
 --DROP TABLE bathroom;
+SELECT babies.firstName, babies.lastName, bathroom.eventType, bathroom.bathroomDateTime, bathroom.bathroomType FROM bathroom
+LEFT JOIN babies ON bathroom.babyID = babies.babyID
+ORDER BY bathroomDateTime
