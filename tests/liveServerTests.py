df669d70141a2e2da760a7835034036e4bee6726
Julia Scott Immer <92125966+Julia-Immer@users.noreply.github.com>
2022-02-23 00:00:28 -0700

    added test directory and started
    will add more Thursday

---
+++ tests/liveServerTests.py
@@ -0,0 +1,21 @@
+import urllib
+from urllib.request import urlopen
+from flask import Flask
+from flask_testing import LiveServerTestCase
+
+class MyTest(LiveServerTestCase):
+    # need to add app config options for testing to views.py file
+    '''
+        def create_app(self):
+            app = Flask(__name__)
+            app.config['TESTING'] = True
+            # Default timeout is 5 seconds
+            app.config['LIVESERVER_TIMEOUT'] = 10
+            return app
+
+        def test_server_is_up_and_running(self):
+            test_url = self.get_server_url() # get the url from the test server launch
+            response = urllib.urlopen(test_url)
+            self.assertEqual(response.code, 200) # 200 is a positive, server is running
+
+    '''
